93ccf3d62043e535e951aa90494ea10f2bf5d6b0
Alexander Medvedev <amedvedev@axmor.com>
2020-04-26 16:51:06 +0700

    types

---
+++ samples/04_types.hs
@@ -0,0 +1,11 @@
+
+factorial :: Integer->Integer
+factorial n = product [1..n]
+
+circumferenceFloat :: Float->Float
+circumferenceFloat r = 2*pi*r
+
+circumferenceDouble :: Double->Double
+circumferenceDouble r = 2*pi*r
+
+swapvec (a,b) = (b,a)
